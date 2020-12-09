0f5a1918390a3edacbadfa61b2de2f89c59cf7e9
Lisaf Iluz <liluz@ebay.com>
2020-12-09 14:27:10 +0200

    os env and ignore files

--- store_me/__init__.py
+++ store_me/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
@@ -5,8 +7,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '2a6352e066e2360e8169b91e4d3e8a5f'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mxpgrlxlxcnfwg:dc13c53fa5d3742a094ecf2e607494270d87ebaadfc5f0c5a23d2c9977c8bb5f@ec2-54-237-155-151.compute-1.amazonaws.com:5432/d3nlt1fikgsico'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
