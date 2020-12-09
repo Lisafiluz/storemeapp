818aa54c2c092e2100a3970f49156b710d34d4ca
Lisaf Iluz <liluz@ebay.com>
2020-12-09 09:16:44 +0200

    db_uri

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from store_me import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- store_me/__init__.py
+++ store_me/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2a6352e066e2360e8169b91e4d3e8a5f'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mxpgrlxlxcnfwg:dc13c53fa5d3742a094ecf2e607494270d87ebaadfc5f0c5a23d2c9977c8bb5f@ec2-54-237-155-151.compute-1.amazonaws.com:5432/d3nlt1fikgsico'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
